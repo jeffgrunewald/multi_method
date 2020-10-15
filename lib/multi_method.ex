7b4b46ce234110d8734b5fed06224362a518f8bd
jeffgrunewald <jeff@blockfi.com>
2020-10-15 11:09:47 -0400

    it sorta works

--- lib/multi_method.ex
+++ lib/multi_method.ex
@@ -1,18 +1,18 @@
 defmodule MultiMethod do
-  @moduledoc """
-  Documentation for MultiMethod.
-  """
+  defmacro defmulti(call, expression)
+  defmacro defmulti({name, _, args}, do: dispatcher_block) do
+    quote do
+      Kernel.def unquote(name)(unquote_splicing(args)) do
+        dispatch_value = unquote(dispatcher_block)
+        unquote(name)(dispatch_value, unquote_splicing(args))
+      end
+    end
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MultiMethod.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defmacro defmethod(call, expression)
+  defmacro defmethod({name, _, args}, do: method_block) do
+    quote do
+      Kernel.defp unquote(name)(unquote_splicing(args)), do: unquote(method_block)
+    end
   end
 end
--- test/multi_method_test.exs
+++ test/multi_method_test.exs
@@ -1,8 +1,38 @@
+import MultiMethod
+
 defmodule MultiMethodTest do
   use ExUnit.Case
   doctest MultiMethod
 
-  test "greets the world" do
-    assert MultiMethod.hello() == :world
+  test "dispatches to simple atoms" do
+    assert Foo.bar(2) == "Because I can : 4"
+    assert Foo.bar(:kitten) == "Unhandled case : :kitten"
   end
+
+  test "dispatches based on computed value" do
+    assert Foo.baz(%{class: "s", name: "John"}) == "Important client John detected!"
+    assert Foo.baz(%{class: "B", net_worth: 65_000}) == "Less valuable client : 65000"
+  end
+end
+
+defmodule Foo do
+  defmulti bar(val) do
+    case val do
+      val when is_integer(val) -> :integer
+      val when is_binary(val) -> :string
+      _ -> :shrug
+    end
+  end
+
+  defmethod bar(:integer, val) do
+    new_val = val * val
+    "Because I can : #{new_val}"
+  end
+  defmethod bar(:string, val), do: "Received a #{val}"
+  defmethod bar(_, val), do: "Unhandled case : #{inspect(val)}"
+
+  defmulti baz(%{class: class} = client), do: class
+
+  defmethod baz("s", %{name: name}), do: "Important client #{name} detected!"
+  defmethod baz("B", %{net_worth: wealth}), do: "Less valuable client : #{wealth}"
 end
